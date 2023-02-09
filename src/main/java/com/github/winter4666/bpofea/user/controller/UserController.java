3fb534e1b076508eacba62b74da814ce38e7d505
winter <winter4666@foxmail.com>
2023-02-09 18:42:22 +0800

    improve minutiae

--- src/main/java/com/github/winter4666/bpofea/user/controller/UserController.java
+++ src/main/java/com/github/winter4666/bpofea/user/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
