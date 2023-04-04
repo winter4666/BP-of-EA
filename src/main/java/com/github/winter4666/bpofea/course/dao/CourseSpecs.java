3f70ee5d76d7a968105e792d08dedba81ed9d1e8
tian <winter4666@foxmail.com>
2023-04-04 17:29:26 +0800

    add @UtilityClass annotation at CourseSpecs

--- src/main/java/com/github/winter4666/bpofea/course/dao/CourseSpecs.java
+++ src/main/java/com/github/winter4666/bpofea/course/dao/CourseSpecs.java
@@ -1,8 +1,10 @@
 package com.github.winter4666.bpofea.course.dao;
 
 import com.github.winter4666.bpofea.course.domain.model.Course;
+import lombok.experimental.UtilityClass;
 import org.springframework.data.jpa.domain.Specification;
 
+@UtilityClass
 public class CourseSpecs {
 
     public static Specification<Course> hasNameStartingWith(String namePrefix) {
