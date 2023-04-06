2448e668652ce32afb45eb3b57d72c7867adc240
tian <winter4666@foxmail.com>
2023-04-06 18:12:19 +0800

    use type-safe query

--- src/main/java/com/github/winter4666/bpofea/course/dao/CourseSpecs.java
+++ src/main/java/com/github/winter4666/bpofea/course/dao/CourseSpecs.java
@@ -1,6 +1,7 @@
 package com.github.winter4666.bpofea.course.dao;
 
 import com.github.winter4666.bpofea.course.domain.model.Course;
+import com.github.winter4666.bpofea.course.domain.model.Course_;
 import lombok.experimental.UtilityClass;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -8,10 +9,10 @@ import org.springframework.data.jpa.domain.Specification;
 public class CourseSpecs {
 
     public static Specification<Course> hasNameStartingWith(String namePrefix) {
-        return Specification.where(namePrefix == null ? null : (root, query, builder) -> builder.like(root.get("name"), namePrefix + "%"));
+        return Specification.where(namePrefix == null ? null : (root, query, builder) -> builder.like(root.get(Course_.NAME), namePrefix + "%"));
     }
 
     public static Specification<Course> hasStateEqualTo(Course.State state) {
-        return Specification.where(state == null ? null : (root, query, builder) -> builder.equal(root.get("state"), state));
+        return Specification.where(state == null ? null : (root, query, builder) -> builder.equal(root.get(Course_.STATE), state));
     }
 }
