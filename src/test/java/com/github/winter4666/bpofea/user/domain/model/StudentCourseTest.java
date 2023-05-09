035840e4df0c9331ce0010e1b410cef2b1a37a22
winter <winter4666@foxmail.com>
2023-05-09 18:37:18 +0800

    add unit test

--- src/test/java/com/github/winter4666/bpofea/user/domain/model/StudentCourseTest.java
+++ src/test/java/com/github/winter4666/bpofea/user/domain/model/StudentCourseTest.java
@@ -4,6 +4,8 @@ import com.github.javafaker.Faker;
 import com.github.winter4666.bpofea.course.datafaker.CourseBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -37,6 +39,19 @@ class StudentCourseTest {
         boolean result = pk1.equals(pk2);
 
         assertThat(result, equalTo(false));
+    }
+
+    @Test
+    void should_return_correct_hash_code_when_invoke_hash_code() {
+        Faker faker = new Faker();
+        long studentId = faker.number().randomNumber();
+        long courseId = faker.number().randomNumber();
+        StudentCourse.PK pk = new StudentCourse.PK(Student.builder().id(studentId).build(),
+                new CourseBuilder().id(courseId).build());
+
+        int result = pk.hashCode();
+
+        assertThat(result, equalTo(Objects.hash(studentId, courseId)));
     }
 
 }
